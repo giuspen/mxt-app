57189d94c7e68d553cb9b18c112fcbe63d0d9573
Giuseppe Penone <giuseppe.penone@solomon-systech.com>
2018-04-06 16:00:37 +0100

    pointing to pull request vianney:android

--- lib/libusbdroid/Android.mk
+++ lib/libusbdroid/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)/code/src/LibUSBDroid/jni
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := libusb_jni.c core.c descriptor.c io.c sync.c os/linux_usbfs.c
+LOCAL_SRC_FILES := core.c descriptor.c hotplug.c io.c libusb_jni.c sync.c os/linux_netlink.c os/linux_usbfs.c os/poll_posix.c os/threads_posix.c
 LOCAL_CFLAGS := -fPIC -DPIC
 LOCAL_MODULE := libusbdroid
 
